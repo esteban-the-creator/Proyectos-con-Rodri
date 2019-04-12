71e6f21cea4c77d91641cb05132f6c6d1c2da0e3
esteban-the-creator <42298103+esteban-the-creator@users.noreply.github.com>
2019-04-11 20:40:39 -0500

    Cambios en el UI
    Cambios menores en UI

---
+++ Cuentas/Assets/AccountManager.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AccountManager : MonoBehaviour
+
+{
+    int currentIndexAccounts;
+    int cod;
+    double valorqueResta;
+    //string EstaCuenta;
+    public InputField nombre;
+    public InputField otroValorRetiro;
+    public InputField saldoInicial;
+    public InputField codigo;
+    public Text titular;
+    public Text saldo;
+    Cuenta[] cuentasBanco = new Cuenta[10]; //anotación 1
+
+
+
+    public void CrearCuenta()
+    {
+        Cuenta NuevaCuenta = new Cuenta(System.Convert.ToDouble(saldoInicial.text), nombre.text); // Anotacion 2
+        cuentasBanco[currentIndexAccounts] = NuevaCuenta;
+        currentIndexAccounts++;
+        titular.text = NuevaCuenta.getTitular();
+
+        string tempSaldo = NuevaCuenta.getCantidad().ToString();
+        saldo.text = tempSaldo;
+    }
+
+    public void ConsultarCuenta()
+    {
+        cod = System.Convert.ToInt32(codigo.text);
+        titular.text = cuentasBanco[cod].getTitular();
+        saldo.text = cuentasBanco[cod].getCantidad().ToString();
+
+    }
+
+    public void Retirar()
+    {
+        cod = System.Convert.ToInt32(codigo.text);
+        valorqueResta = System.Convert.ToDouble(otroValorRetiro.text);
+        double miSaldoActual = System.Convert.ToDouble(saldoInicial.text) ;
+     
+
+        if (valorqueResta > miSaldoActual)
+        {
+            saldo.text = ("No tienes tanto dinero");
+
+        }
+
+        else
+        {
+            double saldoqueQueda = miSaldoActual - valorqueResta;
+            cuentasBanco[cod].setCantidad(saldoqueQueda);
+            saldo.text = cuentasBanco[cod].getCantidad().ToString();
+        }
+
+    }
+}
+
+/*crear un funcion con el onclick para guardar la información de la cuenta como 
+  unanueva y otra funciona que muestra la información de una de las cuentas*/
+
+
+/* En la siguiente línea guardo en una varibale float llamada "temporal" la conversión a flotante (Tosingle) del texto tipo string 
+ingresado en el input field "saldoInicial"; la conversión se hace a través del namespace System, usando la clase convert, llamando 
+el metodo ToSingle y dando en los argumentos el atributo a convertir. PD: para convertir a un double se usa ToDouble
+"float temporal = System.Convert.ToSingle(saldoInicial.text);" */
+
+/* Anotación 1:
+Recordar la sintaxis de un array:
+nombre del tipo de atributo o clase del que se va a hacer una array, corchetes, nombre del array igual (igualdad =) a new tipo de 
+clase o atributo del que estoy haciendo un array y en los ultimos corchetes la cantidad de datos en el array */
+
+/* Anotacion 2: 
+Una solución efectiva cuando no permite usar inputfield.text (nombre.text en este caso) es usar una variable string (EstaCuenta) 
+y asignarle el valor del input field, luego utilizar el setter de la cuenta instanciada (NuevaCuenta.Set())para asignarle el nombre 
+que se guardó en el string "EstaCuenta", sin embargo usamos el constructor para iniciarlizar el valor */
---
+++ Cuentas/Assets/Cuenta.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cuenta 
+
+{
+
+    string titular;
+    double cantidad;
+
+    public Cuenta(double _cantidad, string _titular)
+    {
+        this.cantidad = _cantidad;
+        this.titular = _titular;
+    }
+
+    public Cuenta(string _titular)
+    {
+        this.titular = _titular;
+    }
+
+    /* 2 construct que cumpla con lo anterior, crear los métodos get, set y to string 
+     * 2 métodos epeciales "saldoInicial" será un double cantidad :::se ingresa una cantidad a la cuenta , si la cantidad introducida es negativa no se hara nada 
+     * "retirar" (double cantidad) se retira una cantidad a la cuenta, si restando la cantidd actual a la que nos pasan es negativa, la cantidad de la cuenta pasa aa ser 0*/
+
+
+    public void Ingresar()
+    {
+
+
+    }
+
+    public void Retirar()
+    {
+
+    }
+
+
+    // a partir de este bloque se crean las funciones de instancia get y set para los dos atributos
+
+    public void setCantidad(double _cantidad)
+    {
+        this.cantidad = _cantidad;
+    }
+
+    public double getCantidad()
+    {
+        return this.cantidad;
+    }
+
+    public void setTitular(string _titular)
+    {
+        this.titular = _titular;
+    }
+
+    public string getTitular()
+    {
+        return this.titular;
+    }
+
+
+}
